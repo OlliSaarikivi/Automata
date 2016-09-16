93cb0efb26dd49892284dbd16ea0091d59f3dece
Olli Saarikivi <olli.saarikivi@iki.fi>
2016-09-16 14:38:14 +0300

    Work

---
+++ MinimizationBenchmark/ExplodingTransducer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimizationBenchmark
+{
+    static partial class Transducers
+    {
+        public const string UTF16FixSurrogates = @"
+partial class UTF16FixSurrogates : Transducer<char, char>
+{
+    bool wasHigh = false;
+    char previous;
+
+    bool IsHigh(char c)
+    {
+        return 55296 <= c & c <= 56319;
+    }
+
+    bool IsLow(char c)
+    {
+        return 56320 <= c & c <= 57343;
+    }
+
+    public override IEnumerable<char> Update(char c)
+    {
+        if (wasHigh)
+        {
+            if (IsHigh(c))
+            {
+                yield return '\xFFFD';
+                previous = c;
+            }
+            else
+            {
+                if (IsLow(c))
+                {
+                    yield return previous;
+                }
+                else
+                {
+                    yield return '\xFFFD';
+                }
+                yield return c;
+                previous = (char)0;
+                wasHigh = false;
+            }
+        }
+        else
+        {
+            if (IsHigh(c))
+            {
+                previous = c;
+                wasHigh = true;
+            }
+            else
+            {
+                if (IsLow(c))
+                {
+                    yield return '\xFFFD';
+                }
+                else
+                {
+                    yield return c;
+                }
+                previous = (char)0;
+            }
+        }
+    }
+
+    public override IEnumerable<char> Finish()
+    {
+        if (wasHigh)
+        {
+            yield return '\xFFFD';
+        }
+    }
+}";
+        public const string Exploding = EnglishHuffmanDecoder + UTF8ToUTF16 + UTF16FixSurrogates + @"
+partial class C1 : Composition<EnglishHuffmanDecoder, UTF8ToUTF16> { }
+partial class C1 : Composition<C1, UTF16FixSurrogates> { }";
+    }
+}
--- MinimizationBenchmark/Program.cs
+++ MinimizationBenchmark/Program.cs
@@ -25,22 +25,24 @@ namespace MinimizationBenchmark
 
         static void Main(string[] args)
         {
-            Benchmark(nameof(Transducers.Huffman), Transducers.Huffman);
-            Benchmark(nameof(Transducers.Base64_delta), Transducers.Base64_delta);
-            Benchmark(nameof(Transducers.CSV_max), Transducers.CSV_max);
-            Benchmark(nameof(Transducers.Base64_avg), Transducers.Base64_avg);
-            Benchmark(nameof(Transducers.UTF8_lines), Transducers.UTF8_lines);
-            Benchmark(nameof(Transducers.CC_id), Transducers.CC_id);
-            Benchmark(nameof(Transducers.CHSI_cancer), Transducers.CHSI_cancer);
-            Benchmark(nameof(Transducers.CHSI_births), Transducers.CHSI_births);
-            Benchmark(nameof(Transducers.CHSI_deaths), Transducers.CHSI_deaths);
-            Benchmark(nameof(Transducers.SBO_employees), Transducers.SBO_employees);
-            Benchmark(nameof(Transducers.SBO_receipts), Transducers.SBO_receipts);
-            Benchmark(nameof(Transducers.SBO_payroll), Transducers.SBO_payroll);
-            Benchmark(nameof(Transducers.TPC_DI_SQL), Transducers.TPC_DI_SQL);
-            Benchmark(nameof(Transducers.PIR_proteins), Transducers.PIR_proteins);
-            Benchmark(nameof(Transducers.DBLP_oldest), Transducers.DBLP_oldest);
-            Benchmark(nameof(Transducers.MONDIAL_pop), Transducers.MONDIAL_pop);
+            //Benchmark(nameof(Transducers.Huffman), Transducers.Huffman);
+            //Benchmark(nameof(Transducers.Base64_delta), Transducers.Base64_delta);
+            //Benchmark(nameof(Transducers.CSV_max), Transducers.CSV_max);
+            //Benchmark(nameof(Transducers.Base64_avg), Transducers.Base64_avg);
+            //Benchmark(nameof(Transducers.UTF8_lines), Transducers.UTF8_lines);
+            //Benchmark(nameof(Transducers.CC_id), Transducers.CC_id);
+            //Benchmark(nameof(Transducers.CHSI_cancer), Transducers.CHSI_cancer);
+            //Benchmark(nameof(Transducers.CHSI_births), Transducers.CHSI_births);
+            //Benchmark(nameof(Transducers.CHSI_deaths), Transducers.CHSI_deaths);
+            //Benchmark(nameof(Transducers.SBO_employees), Transducers.SBO_employees);
+            //Benchmark(nameof(Transducers.SBO_receipts), Transducers.SBO_receipts);
+            //Benchmark(nameof(Transducers.SBO_payroll), Transducers.SBO_payroll);
+            //Benchmark(nameof(Transducers.TPC_DI_SQL), Transducers.TPC_DI_SQL);
+            //Benchmark(nameof(Transducers.PIR_proteins), Transducers.PIR_proteins);
+            //Benchmark(nameof(Transducers.DBLP_oldest), Transducers.DBLP_oldest);
+            //Benchmark(nameof(Transducers.MONDIAL_pop), Transducers.MONDIAL_pop);
+
+            Benchmark(nameof(Transducers.Exploding), Transducers.Exploding);
 
             var line = Console.ReadLine();
         }
