42d38353a249732e313891472f4b6d2fb17acb1a
Olli Saarikivi <olli.saarikivi@iki.fi>
2017-02-20 11:14:23 +0200

    Add missing files from runtime project removal

---
+++ src/CSharpFrontend/Transducer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Automata.CSharpFrontend
+{
+    public abstract class Transducer<TIn, TOut>
+    {
+        public abstract IEnumerable<TOut> Update(TIn datum);
+        public virtual IEnumerable<TOut> Finish() { yield break; }
+    }
+
+    public abstract class Composition<TInner, TOuter>
+    {
+    }
+
+    public abstract class SpecialTransducer { }
+}
